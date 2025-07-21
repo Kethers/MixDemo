93a7a3889fa7e1a01db9e17ea31646179e1ccf09
Kethers <49612587+Kethers@users.noreply.github.com>
2025-07-21 18:15:41 +0800

    [Feat]upload Entry scene with basic hotupdate functionality

---
+++ Assets/Editor/HybridCLRHelper.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using HybridCLR.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor.Tools
+{
+    [InitializeOnLoad]
+    public class HybridCLRHelper
+    {
+        private static string[] _hotUpdateDllNames =
+        {
+            "HotUpdate.dll",
+        };
+
+        private static List<string> _patchMetaDataDllNames;
+
+        private const string YooCodeHotUpdateDir = "Assets/YooDownloadRes/Code";
+        
+        static HybridCLRHelper()
+        {
+            _patchMetaDataDllNames = new(SettingsUtil.AOTAssemblyNames);
+            for (var i = 0; i < _patchMetaDataDllNames.Count; i++)
+            {
+                var dllName = _patchMetaDataDllNames[i];
+                if (dllName.EndsWith(".dll"))
+                {
+                    dllName = dllName.Substring(0, dllName.Length - 4);
+                }
+                _patchMetaDataDllNames[i] = dllName + ".dll";
+            }
+        }
+        
+        [MenuItem("HybridCLR/Custom/Quick copy dll")]
+        public static void CopyHybridCLRGeneratedDllToHotUpdateFolder()
+        {
+            var genDir = SettingsUtil.GetHotUpdateDllsOutputDirByTarget(EditorUserBuildSettings.activeBuildTarget);
+            var metaDllDir = SettingsUtil.GetAssembliesPostIl2CppStripDir(EditorUserBuildSettings.activeBuildTarget);
+            if (!Directory.Exists(genDir))
+            {
+                Debug.LogError($"Directory {genDir} does not exist. Please compile Dll first.");
+                return;
+            }
+            if (!Directory.Exists(metaDllDir))
+            {
+                Debug.LogError($"Directory {metaDllDir} does not exist. Please compile Dll first.");
+                return;
+            }
+
+            CopyDllToDestWithBytesPostfix(_hotUpdateDllNames, genDir, YooCodeHotUpdateDir);
+            CopyDllToDestWithBytesPostfix(_patchMetaDataDllNames, metaDllDir, YooCodeHotUpdateDir);
+        }
+
+        private static void CopyDllToDestWithBytesPostfix(IList<string> dllNames, string srcDir, string dstDir)
+        {
+            foreach (var dllName in dllNames)
+            {
+                string srcFile = Path.Combine(srcDir, Path.GetFileName(dllName));
+                string destFile = Path.Combine(dstDir, Path.GetFileName(dllName) + ".bytes");
+                File.Copy(srcFile, destFile, true);
+                Debug.Log($"Copied {dllName} to {destFile}");
+            }
+            AssetDatabase.Refresh();
+        }
+    }
+}
---
+++ Assets/HybridCLRGenerate/AOTGenericReferences.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+public class AOTGenericReferences : UnityEngine.MonoBehaviour
+{
+
+	// {{ AOT assemblies
+	public static readonly IReadOnlyList<string> PatchedAOTAssemblyList = new List<string>
+	{
+		"UnityEngine.CoreModule.dll",
+		"mscorlib.dll",
+	};
+	// }}
+
+	// {{ constraint implement type
+	// }} 
+
+	// {{ AOT generic types
+	// System.Action<int,int>
+	// System.Action<object>
+	// System.Collections.Generic.ArraySortHelper<object>
+	// System.Collections.Generic.Comparer<object>
+	// System.Collections.Generic.ICollection<object>
+	// System.Collections.Generic.IComparer<object>
+	// System.Collections.Generic.IEnumerable<object>
+	// System.Collections.Generic.IEnumerator<object>
+	// System.Collections.Generic.IList<object>
+	// System.Collections.Generic.List.Enumerator<object>
+	// System.Collections.Generic.List<object>
+	// System.Collections.Generic.ObjectComparer<object>
+	// System.Collections.ObjectModel.ReadOnlyCollection<object>
+	// System.Comparison<object>
+	// System.Predicate<object>
+	// }}
+
+	public void RefMethods()
+	{
+		// object& System.Runtime.CompilerServices.Unsafe.As<object,object>(object&)
+		// System.Void* System.Runtime.CompilerServices.Unsafe.AsPointer<object>(object&)
+		// object UnityEngine.Component.GetComponent<object>()
+		// object UnityEngine.GameObject.GetComponent<object>()
+		// object UnityEngine.Object.FindObjectOfType<object>()
+		// object[] UnityEngine.Object.FindObjectsOfType<object>()
+		// object[] UnityEngine.Object.FindObjectsOfType<object>(bool)
+		// object UnityEngine.Object.Instantiate<object>(object)
+		// object UnityEngine.Object.Instantiate<object>(object,UnityEngine.Transform)
+		// object UnityEngine.Object.Instantiate<object>(object,UnityEngine.Transform,bool)
+		// object UnityEngine.Object.Instantiate<object>(object,UnityEngine.Vector3,UnityEngine.Quaternion)
+		// object[] UnityEngine.Resources.ConvertObjects<object>(UnityEngine.Object[])
+	}
+}
